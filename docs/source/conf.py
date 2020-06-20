706b3c41ea09039591c1ef6c65c85f741c7c6fe8
David Mas-Ponte <david.mas@irbbarcelona.org>
2020-06-20 20:23:11 +0200

    update 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-## see this https://github.com/readthedocs/readthedocs.org/issues/2569
-master_doc = 'index'
+
 
 # -- Project information -----------------------------------------------------
 
@@ -54,3 +53,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+## see this https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
